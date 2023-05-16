c30ba475d17d2f6aa148f8fd930efb6024f71e7c
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-16 13:04:16 +0200

    base structure header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <Header />
   <router-view></router-view>
 </template>
 
 <script>
+import Header from './components/Header/Header.vue'
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header
+  },
 };
 </script>
 
