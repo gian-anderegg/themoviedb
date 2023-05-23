fc5ba3ded234b95e275d8ce2832613f6bb00cc2f
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-23 11:11:29 +0200

    added some css

--- src/pages/MoviePage/MoviePage.js
+++ src/pages/MoviePage/MoviePage.js
@@ -1,7 +1,15 @@
 export default{
     name: 'MoviePage',
     data(){
-      return{}
+      return{
+        movie: {
+            image: "",
+            title: "This is the movie title",
+            date: "12. 12 2016",
+            rating: 4,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis earum molestiae ea rerum facere cumque cupiditate ipsa itaque, excepturi iste consequatur, debitis, dolores nostrum magni accusamus beatae quaerat enim asperiores."
+        }
+      }
     },
     methods:{
     }
--- src/pages/MoviePage/MoviePage.vue
+++ src/pages/MoviePage/MoviePage.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="movie-page">
-    <div class="icon"></div>
+    <div class="icon">icon</div>
     <div class="movie-info">
         <img class="movie-image" src="@/assets/Gradient.jpeg" />
         <div class="movie-data">
-            <span class="movie-title"></span>
-            <span class="movie-release-date"></span>
-            <div class="movie-rating"></div>
+            <span class="movie-title">{{movie.title}}</span>
+            <span class="movie-release-date">{{movie.date}}</span>
+            <div class="movie-rating">{{movie.rating}}</div>
             <div class="move-paragraph">
                 <span class="movie-description-title">Description</span>
-                <span class="movie-description"></span>
+                <span class="movie-description">{{movie.description}}</span>
             </div>
         </div>
     </div>
