48e49da1fe2a1e6f66635c722e6e445689a9bf1e
Gian Anderegg <gian051004@gmail.com>
2023-05-23 13:24:26 +0200

    renamed folder

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <h1>The Movie DB</h1>
-  <film-section></film-section>
-  <categories @selected-state-change="selectedStateChange" category_name="Hello"></categories> 
-</template>
+</film-section></template>
 
-    <script src="./Home.js" />
+<script src="./Home.js" />
 
     <style src="./Home.css" scoped />
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>The Movie DB</h1>
+    <filmSection />
+    <i class="bi bi-heart"></i>
+  </div>
+</template>
+
+<script src="./Home.js" />
+
+<style src="./Home.css" scoped />
