f8a998cc1e80ee9d101e20edec1ae7ad0747b6e1
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-23 13:13:57 +0200

    finished movie page

--- src/pages/MoviePage/MoviePage.vue
+++ src/pages/MoviePage/MoviePage.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="movie-page">
-    <div class="icon">icon</div>
+    <div class="icon"></div>
     <div class="movie-info">
         <img class="movie-image" src="@/assets/Gradient.jpeg" />
         <div class="movie-data">
             <span class="movie-title">{{movie.title}}</span>
             <span class="movie-release-date">{{movie.date}}</span>
-            <div class="movie-rating">{{movie.rating}}</div>
-            <div class="move-paragraph">
+            <div class="movie-rating">
+              <span v-for="n in movie.rating" :key="n" class="movie-rating-hearts"></span>
+            </div>
+            <div class="movie-paragraph">
                 <span class="movie-description-title">Description</span>
                 <span class="movie-description">{{movie.description}}</span>
             </div>
@@ -15,7 +17,7 @@
     </div>
     <div class="movie-trailer">
         <span class="trailer-text">View the trailer here</span>
-        <iframe src="" />
+        <iframe class="trailer-video" src="https://www.youtube.com/watch?v=vqs_0W-MSB0" frameborder="0" allowfullscreen></iframe>
     </div>
   </div>
 </template>
