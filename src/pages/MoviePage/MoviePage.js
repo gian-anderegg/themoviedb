f68b426140090362b720112e9a1a4e6cdd01f7ff
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-23 10:48:16 +0200

    initial setup for movie page

---
+++ src/pages/MoviePage/MoviePage.js
@@ -0,0 +1,8 @@
+export default{
+    name: 'MoviePage',
+    data(){
+      return{}
+    },
+    methods:{
+    }
+}
---
+++ src/pages/MoviePage/MoviePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="movie-page">
+    <div class="icon"></div>
+    <div class="movie-info">
+        <img class="movie-image" src="@/assets/Gradient.jpeg" />
+        <div class="movie-data">
+            <span class="movie-title"></span>
+            <span class="movie-release-date"></span>
+            <div class="movie-rating"></div>
+            <div class="move-paragraph">
+                <span class="movie-description-title">Description</span>
+                <span class="movie-description"></span>
+            </div>
+        </div>
+    </div>
+    <div class="movie-trailer">
+        <span class="trailer-text">View the trailer here</span>
+        <iframe src="" />
+    </div>
+  </div>
+</template>
+
+<script src="./MoviePage.js" />
+
+<style src="./MoviePage.css" scoped />
