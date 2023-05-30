b741f6ef36989a18fad541d6b4c972e2ca93ff48
Neo Fanetti <neo.fanetti@bbzsogr.ch>
2023-05-30 22:05:53 +0200

    fix(i18n-emit-error): added transform-define plugin to babel config (not solved)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    ['@vue/cli-plugin-babel/preset', {
+      plugins: [
+        ['transform-define', {
+          '__VUE_I18N_FULL_INSTALL__': false,
+          '__VUE_I18N_LEGACY_API__': false,
+          '__VUE_I18N_PROD_DEVTOOLS__': false,
+        }],
+      ],
+    }]
+  ],
+};
