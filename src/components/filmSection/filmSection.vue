8d49cf160f44a23fdc4822ad351c831dc118066e
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-30 20:15:59 +0200

    changed displaying of movie title
    movie title is now beeing displayed on hovering over movie

--- src/components/filmSection/filmSection.vue
+++ src/components/filmSection/filmSection.vue
@@ -3,7 +3,7 @@
         <div class="film-section-wrapper">
             <div v-for="movie in movies" :key="movie">
                 <div class="grid-item">
-                    <h4>{{ movie.title }}</h4>
+                    <h4 class="movie-title">{{ movie.title }}</h4>
                     <div class="image-container">
                         <img class="responsive-imgs" :src="'https://image.tmdb.org/t/p/original' + movie.poster_path">
                     </div>
