046411eb8befb3c7527380373e33e7bdecfa0204
Gian Anderegg <gian051004@gmail.com>
2023-05-30 11:36:10 +0200

    heart animation

--- src/components/filmSection/filmSection.js
+++ src/components/filmSection/filmSection.js
@@ -1,6 +1,22 @@
 export default {
     name: 'filmSection',
     props: {
-        movies: Array,
+      movies: Array,
     },
-};
+    data() {
+      return {
+        clickedMovies: [], 
+      };
+    },
+    methods: {
+      isMovieClicked(movie) {
+        return this.clickedMovies.includes(movie);
+      },
+      handleClick(movie) {
+        if (!this.isMovieClicked(movie)) {
+          this.clickedMovies.push(movie);
+        }
+      },
+    },
+  };
+  
