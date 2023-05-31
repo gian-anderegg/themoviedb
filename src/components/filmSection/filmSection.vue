3e6ebe3334a2ad22789874fd0ffcf524914c47ee
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-05-31 09:59:38 +0200

    movie animation workover
    made animation trigger when image is loaded

--- src/components/filmSection/filmSection.vue
+++ src/components/filmSection/filmSection.vue
@@ -2,10 +2,10 @@
     <div>
         <div class="film-section-wrapper">
             <div v-for="movie in movies" :key="movie">
-                <div class="grid-item">
+                <div class="grid-item" ref="gridItem">
                     <h4 class="movie-title">{{ movie.title }}</h4>
                     <div class="image-container">
-                        <img class="responsive-imgs" :src="'https://image.tmdb.org/t/p/original' + movie.poster_path">
+                        <img class="responsive-imgs" :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" @load="loadMovieAnimation">
                     </div>
                     <div class="gardiant-container"></div>
                     <span class="heart-icon" :class="{ filled: isMovieClicked(movie) }" @click="handleClick(movie)"></span>
