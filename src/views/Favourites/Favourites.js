8f414ad1c04db9cd4eb9fcc2e10111938fd98c1b
lukasBBZ <lukasmartin.folch@bbzsogr.ch>
2023-06-06 11:46:38 +0200

    added search function on discover and favourites

--- src/views/Favourites/Favourites.js
+++ src/views/Favourites/Favourites.js
@@ -11,9 +11,20 @@ export default {
             //isLoading: false,
         }
     },
+    methods: {
+        search(searchQuery) {
+            this.movies = this.movies.filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase()));
+        },
+        clear() {
+            this.loadMovies();
+        },
+        loadMovies() {
+            const favourites = localStorage.getItem('favourites');
+            if (favourites) this.movies = JSON.parse(favourites);
+        },
+    },
     async mounted() {
         // load liked movies from local storage (not from api)
-        const favourites = localStorage.getItem('favourites');
-        if (favourites) this.movies = JSON.parse(favourites);
+        this.loadMovies();
     }
 }
